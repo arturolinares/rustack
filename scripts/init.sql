1d55a2e4d8ea31cad9aa3729657ae284f3b8ad5b
Arturo Linares <arturo.linares@gmail.com>
2021-05-20 19:56:30 -0500

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,3 +1,3 @@
-CREATE user highlights WITH CREATEDB PASSWORD 'highlights';
-CREATE DATABASE highlights;
-GRANT all privileges ON DATABASE highlights TO highlights;
+CREATE user ~db_user~ WITH CREATEDB PASSWORD '~db_pass~';
+CREATE DATABASE ~db_name~;
+GRANT all privileges ON DATABASE ~db_name~ TO ~db_user~;
